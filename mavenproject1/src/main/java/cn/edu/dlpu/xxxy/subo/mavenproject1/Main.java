a231136d6b3bde4abf6558e698d5c4467e388b3c
Administrator <Administrator@PC-20180705RBZA>
2019-03-23 13:10:56 +0800

    helloworld

---
+++ mavenproject1/src/main/java/cn/edu/dlpu/xxxy/subo/mavenproject1/Main.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cn.edu.dlpu.xxxy.subo.mavenproject1;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static String input(String prompt) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(prompt + ":");
+        String input = scanner.next();
+        return input;
+    }
+
+    public static void main(String[] args) {
+        String accountID = input("AccountID");
+        String password = input("Password");
+        if (("CCB62888".equals(accountID)) && ("123456".equals(password))) {
+            System.out.println("Login Success");
+            System.out.println(accountID + "账户余额: " + 10010 + "$");
+        } else {
+            System.out.println("Login Failed");
+        }
+    }
+}
